863d79adb166daf47ff152d66f9e44d6982fd281
Eugene Yew Chan (NCS) <eugene.yewchan@ncs.com.sg>
2021-10-27 15:20:23 +0800

    Add typing to handler and test case

--- src/__tests__/functions/hello.spec.ts
+++ src/__tests__/functions/hello.spec.ts
@@ -1,21 +1,27 @@
 import { main as lambdaFunc } from '@functions/hello/handler';
 import { mockLambdaContext } from '@libs/mocks';
-import { APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyResult, Callback } from 'aws-lambda';
 
 describe('Testing lambda function handlers', () => {
   test('hello', (done) => {
     const event = require('@functions/hello/mock.json');
 
-    lambdaFunc(event, mockLambdaContext, (err, res: APIGatewayProxyResult) => {
-      if (err) {
-        done(err);
-      } else {
-        const body = JSON.parse(res.body);
+    const callback: Callback<APIGatewayProxyResult> = (err, res) => {
+      try {
+        if (err) {
+          done(err);
+        } else if (res !== undefined) {
+          const body = JSON.parse(res.body);
 
-        expect(res.statusCode).toBe(200);
-        expect(body.message).toEqual('Hello Frederic!');
-        done();
+          expect(res.statusCode).toBe(200);
+          expect(body.message).toEqual('Hello Frederic!');
+          done();
+        }
+      } catch (err) {
+        done(err);
       }
-    });
+    };
+
+    lambdaFunc(event, mockLambdaContext, callback);
   });
 });
--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,12 +1,13 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
+import { APIGatewayProxyResult } from 'aws-lambda';
 
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
-) => {
+): Promise<APIGatewayProxyResult> => {
   return formatJSONResponse({
     message: `Hello ${event.body.name}!`,
     event,
